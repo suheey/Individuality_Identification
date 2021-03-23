464ca1c608ade5d8aabedb289d53454571ab0fe4
suhee <suheeyoon@makinarocks.ai>
2021-03-23 13:06:51 +0900

    refactoring

--- age.py
+++ age.py
@@ -1,29 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
-import tables
-import os
-import json 
-import glob
-import cv2
+import tables, os, json, glob, cv2
+
 from tqdm import tqdm
 from jamo import j2hcj, h2j
 from IPython.display import Image
 import matplotlib.pyplot as plt
-
-
-# In[2]:
-
+import itertools
 
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-import itertools
 
-from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+from keras.utils.np_utils import to_categorical 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization
 from keras.optimizers import RMSprop,Adam
@@ -31,13 +18,9 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ReduceLROnPlateau
 
 
-# In[4]:
-
-
 img_list = glob.glob("code/01_handwriting_syllable_images/1_syllable/*.png") 
 
 # resize image
-
 resized_img = []
 image_id = []
 
@@ -50,15 +33,11 @@ for i in tqdm(range(len(img_list))):
         resized_img.append(newsize)
 
 
-# In[5]:
-
-
 get_ipython().run_line_magic('matplotlib', 'inline')
 plt.imshow(resized_img[1])
 
 
-# In[7]:
-
+# Match Image and json file based on Image_ID
 
 info_file = open('handwriting_data_info1.json', encoding='UTF8').read() 
 
@@ -77,9 +56,6 @@ for i in range(data_N):
         syllable_id.append(infos['annotations'][i]['id'])
         syllable_id_age.append(infos['annotations'][i]['attributes']['age'])
         syllable_id_text.append(infos['annotations'][i]['text'])
-
-
-# In[9]:
 
 
 syage = []
@@ -119,11 +95,8 @@ thr_erl = [0,0,1,0]
 thr_lat = [0,0,0,1] 
 
 
-# In[11]:
-
-
+# categorize age range
 v_age = []
-
 for i in range(len(syage_sel)):
     if 22<= int(syage_sel[i]) <= 25 :
         v_age.append(twe_erl)
@@ -141,16 +114,9 @@ v_age=np.asarray(v_age)
 print("::Number of age list ", len(v_age))
 
 
-# In[12]:
-
-
 
 norm_img=[resized_img_sel[i] / 255.0 for i in range(len(resized_img_sel))]
 print("norm_img shape: ",len(norm_img))
-
-
-# In[13]:
-
 
 
 rsh_img = [norm_img[i].reshape(128,128,1) for i in range(len(norm_img))]
@@ -158,24 +124,11 @@ rsh_img=np.asarray(rsh_img)
 print("rsh_img shape: ",rsh_img.shape)
 
 
-# In[14]:
 
-
+#tran, test split
 X_train, X_test, Y_train, Y_test = train_test_split(rsh_img, v_age, test_size = 0.1, random_state=2)
 X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=2)
-print("X_train shape",X_train.shape)
-print("X_val shape",X_val.shape)
-print("X_test shape",X_test.shape)
-print("Y_train shape",Y_train.shape)
-print("Y_val shape",Y_val.shape)
-print("Y_test shape",Y_test.shape)
 
-# train 6714 10738
-# val 697 1243
-# test 849 1306
-
-
-# In[8]:
 
 
 def _permutation(set):
@@ -186,10 +139,7 @@ def _permutation(set):
     return set
 
 
-# In[15]:
-
-
-#Model
+# CNN Model
 model = Sequential()
 
 model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
@@ -214,45 +164,25 @@ model.add(Dropout(0.5))
 model.add(Dense(4, activation = "softmax"))
 
 
-# In[16]:
-
 
 epochs = 10  # for better result increase the epochs
 batch_size = 250
 
 
-# In[2]:
-
-
 # Define the optimizer
 optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
-
-
-# In[18]:
-
-
-
 model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
 
-
-# In[19]:
-
-
+# Model Fitting
 history = model.fit(X_train,Y_train, batch_size=batch_size,
                               epochs = epochs, validation_data = (X_val,Y_val))
 
 
-# In[20]:
-
-
+# Evaluate Performance
 score, acc=model.evaluate(X_test, Y_test, batch_size=batch_size)
 print(score, acc)
 
-
-# In[22]:
-
-
-
+# plot loss
 plt.plot(history.history['val_loss'], color='b', label="validation loss")
 plt.title("Test Loss")
 plt.xlabel("Number of Epochs")
